d2603329207cf55452ff9d9356a5772170791edd
katrinalyyy <k44trinalyyy@mail.ru>
2022-05-06 15:32:19 +0300

    все работает кроме страниц со статьями, блога, закгрузки постов

--- blog/main/routes.py
+++ blog/main/routes.py
@@ -3,41 +3,41 @@ from flask import Blueprint, render_template
 main = Blueprint('main', __name__)
 
 
-@main.route('/')  # 1
+@main.route('/')
 def home():
     return render_template('index.html', title='Главная')
 
 
-@main.route('/capacitor_page')  # 2
+@main.route('/capacitor_page')
 def capacitor_page():
     return render_template('capacitor_page.html')
 
 
-@main.route('/blog')  # 3
+@main.route('/blog')
 def blog():
-    return render_template('blog.html', title='Блог')   #&&&&&&&&&&
+    return render_template('blog.html', title='Блог')
 
 
-@main.route('/dielectric_page')  # 4
+@main.route('/dielectric_page')
 def dielectric_page():
     return render_template('dielectric_page.html')
 
 
-@main.route('/formula_page')  # 5
+@main.route('/formula_page')
 def formula_page():
     return render_template('formula_page.html')
 
 
-@main.route('/guides_page')  # 6
+@main.route('/guides_page')
 def guides_page():
     return render_template('guides_page.html')
 
 
-@main.route('/notes_page')  # 7
+@main.route('/notes_page')
 def notes_page():
     return render_template('notes_page.html')
 
 
-@main.route('/tickets_page')  # 8
+@main.route('/tickets_page')
 def tickets_page():
     return render_template('tickets_page.html')
